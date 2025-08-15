6ab704cc37f5dd2c3449a336dd30881a827c9b8b
Nater <natomato@users.noreply.github.com>
2025-08-15 15:07:41 -0400

    Enabled astrocore and astrolsp files
    Added L and H for next and prev buffer

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,9 +1,5 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
--- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
---       as this provides autocomplete and documentation while editing
 
 ---@type LazySpec
 return {
@@ -58,6 +54,11 @@ return {
       -- first key is the mode
       n = {
         -- second key is the lefthand side of the map
+
+        -- navigate buffer tabs with `H` and `L`
+        L = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
+        H = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+
 
         -- navigate buffer tabs
         ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
